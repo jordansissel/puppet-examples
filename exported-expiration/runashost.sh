d0bca48744f2131e1ab4c4a83e1998649a5637cd
Jordan Sissel <jls@semicomplete.com>
2010-11-04 00:14:07 -0700

    set -x

--- exported-expiration/runashost.sh
+++ exported-expiration/runashost.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 2 ] ; then
   exit 1
 fi
 
-#set -x
+set -x
 
 # I set FACTER_fqdn and FACTER_ipaddress_eth0 so we can easily pretend to be another
 # host. This allows us to show this demo without requiring a run on different hosts.
